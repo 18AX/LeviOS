eb03b9f9f3f715df5e80a44d78d56a09b3e55087
Alex Lev <levzx18@gmail.com>
2021-12-27 22:48:47 +0100

    memory: add functions to get physical address of a kernel/hhdm address

--- include/levi/memory/memory.h
+++ include/levi/memory/memory.h
@@ -5,10 +5,21 @@
 
 #include "levi/types.h"
 
+#define KERNEL_ADDRESS 0xffffffff80000000
+#define HHDM_ADDRESS 0xffff800000000000
+
 u64 align_up(u64 ptr);
 
 u64 align_down(u64 ptr);
 
 u32 is_align(u64 ptr);
+
+u64 kaddress_to_phys(u64 addr);
+
+u64 phys_to_kaddress(u64 addr);
+
+u64 hhdm_to_phys(u64 addr);
+
+u64 phys_to_hhdm(u64 addr);
 
 #endif
--- kernel/memory/memory.c
+++ kernel/memory/memory.c
@@ -14,3 +14,23 @@ u32 is_align(u64 ptr)
 {
     return (ptr & (PAGE_SIZE - 1)) == 0;
 }
+
+u64 kaddress_to_phys(u64 addr)
+{
+    return addr - KERNEL_ADDRESS;
+}
+
+u64 phys_to_kaddress(u64 addr)
+{
+    return addr + KERNEL_ADDRESS;
+}
+
+u64 hhdm_to_phys(u64 addr)
+{
+    return addr - HHDM_ADDRESS;
+}
+
+u64 phys_to_hhdm(u64 addr)
+{
+    return addr + HHDM_ADDRESS;
+}
