abeb3a7681065c503d6e0a4b2a008d1a355fe051
Alex Lev <levzx18@gmail.com>
2022-07-15 15:06:31 +0200

    syscall: add syscall exit

---
+++ include/levi/syscall/exit.h
@@ -0,0 +1,10 @@
+#ifndef EXIT_HEADER
+#define EXIT_HEADER
+
+#include <levi/proc/process.h>
+#include <levi/types.h>
+
+u64 sys_exit(proc_t *proc, u64 status_code, u64 unused0, u64 unused1,
+             u64 unused2);
+
+#endif
---
+++ kernel/syscall/exit.c
@@ -0,0 +1,15 @@
+#include <levi/syscall/exit.h>
+#include <levi/syscall/syscall.h>
+
+u64 sys_exit(proc_t *proc, u64 status_code, u64 unused0, u64 unused1,
+             u64 unused2)
+{
+    (void)status_code; /** For now we do nothing with the status code **/
+    (void)unused0;
+    (void)unused1;
+    (void)unused2;
+
+    proc_destroy(proc);
+
+    return 0;
+}
