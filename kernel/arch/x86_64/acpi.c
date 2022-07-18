5734caf0e2bad098f9d4148fe8a2e280a2215d8f
Alex Lev <levzx18@gmail.com>
2022-07-18 22:56:06 +0200

    acpi: fix page fault, move all pointers to higer half memory.

--- kernel/arch/x86_64/acpi.c
+++ kernel/arch/x86_64/acpi.c
@@ -1,4 +1,5 @@
 #include <levi/arch/x86_64/acpi.h>
+#include <levi/memory/memory.h>
 #include <levi/utils/string.h>
 
 static struct rsdp2 *rsdp = NULL;
@@ -21,10 +22,10 @@ STATUS acpi_init(struct stivale2_struct *boot_info)
 
     if (rsdp->rsdp.revision != 0) // ACPI version > 1
     {
-        xsdt = (struct xsdt *)rsdp->xsdt_address;
+        xsdt = (struct xsdt *)phys_to_hhdm(rsdp->xsdt_address);
     }
 
-    rsdt = (struct rsdt *)((u64)rsdp->rsdp.rsdt_address);
+    rsdt = (struct rsdt *)phys_to_hhdm((u64)rsdp->rsdp.rsdt_address);
 
     return SUCCESS;
 }
@@ -37,7 +38,7 @@ static struct acpi_sdt_header *__xsdt_find(const char sig[4])
     for (u64 i = 0; i < xsdt_len; ++i)
     {
         struct acpi_sdt_header *h =
-            (struct acpi_sdt_header *)xsdt->xsdt_table[i];
+            (struct acpi_sdt_header *)phys_to_hhdm(xsdt->xsdt_table[i]);
 
         if (strncmp(sig, h->signature, 4) == 0)
         {
@@ -56,7 +57,7 @@ static struct acpi_sdt_header *__rsdt_find(const char sig[4])
     for (u64 i = 0; i < rsdt_len; ++i)
     {
         struct acpi_sdt_header *h =
-            (struct acpi_sdt_header *)((u64)rsdt->rsdt_table[i]);
+            (struct acpi_sdt_header *)phys_to_hhdm((u64)rsdt->rsdt_table[i]);
 
         if (strncmp(sig, h->signature, 4) == 0)
         {
